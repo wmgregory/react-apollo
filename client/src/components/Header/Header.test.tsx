d4bf7584bd0cf98fa629dd3393841877ae6439eb
William Gregory <william.gregory@acadiasoft.com>
2020-02-13 12:46:21 +0000

    refactor(Header): move to own dir

---
+++ client/src/components/Header/Header.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { Header } from './Header';
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<Header title="New Title" />);
+  expect(wrapper).toBeDefined();
+  expect(wrapper.debug()).toMatchSnapshot();
+});
