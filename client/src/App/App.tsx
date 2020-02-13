e926fa9782832f49a9694675922bdaa01287483a
William Gregory <william.gregory@acadiasoft.com>
2020-02-13 17:02:28 +0000

    feat(App): add Material UI and styled components

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
 import './App.css';
+import theme from './App.theme';
 
 import { client } from '../services/client';
 import { Header } from '../components/Header';
 import { RandomNumber } from '../components/RandomNumber';
 
+const StyledApp = styled('div')`
+  text-align: center;
+  min-height: 100vh;
+`;
+
 export const App: React.FC = () => (
   <ApolloProvider client={client}>
-    <div className='App'>
-      <Header title='React Apollo demo' />
-      <RandomNumber />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <StyledApp>
+        <Header title='React Apollo demo' />
+        <RandomNumber />
+      </StyledApp>
+    </ThemeProvider>
+    ,
   </ApolloProvider>
 );
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface HeaderProps {
   title: string;
 }
 
+const StyledHeader = styled('header')`
+  display: flex;
+  flex-direction: column;
+
+  color: white;
+  background-color: #282c34;
+`;
+
+const StyledHeading = styled('h1')`
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+`;
+
 export const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <header className='App-header'>
-      <h1>{title}</h1>
-    </header>
+    <StyledHeader>
+      <StyledHeading>{title}</StyledHeading>
+    </StyledHeader>
   );
 };
